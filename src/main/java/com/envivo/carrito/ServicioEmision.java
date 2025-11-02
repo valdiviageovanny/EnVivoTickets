bd646482746adb6074a2ed7d094a1ad09d131a63
Geovanny Valdivia <150383421+valdiviageovanny@users.noreply.github.com>
2025-11-02 00:08:46 -0500

    Carrito, notificaciones y common

---
+++ src/main/java/com/envivo/carrito/ServicioEmision.java
@@ -0,0 +1,14 @@
+package com.envivo.carrito;
+
+import com.envivo.inventario.Ticket;
+import com.envivo.inventario.Orden;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicioEmision {
+    public List<Ticket> emitir(Orden orden) {
+        //Emitir ticket
+        return new ArrayList<>();
+    }
+}
